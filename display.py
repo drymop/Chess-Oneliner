e26510f9a1b405fa0c31f03347221911da7d8fe0
lmtuan <lmtuan98@gmail.com>
2020-02-20 22:44:33 -0500

    DONE BABY

--- display.py
+++ display.py
@@ -3,7 +3,7 @@ from move_gen_2 import GM as gen_move
 from notation import get_move_notation
 from tree_search_2 import NM as negamax
 
-PLAYERS = ["White", "Black"]
+PLAYERS = ["White","Black"]
 DEPTH = 3
 
 def get_user_move(b,moves):
@@ -25,7 +25,7 @@ def get_move(b,moves,color):
 
 
 def main():
-    b = [[4,8,6,2,0,6,8,4],[10]*8,[-1]*8,[-1]*8,[-1]*8,[-1]*8,[11]*8,[5,9,7,3,1,7,9,5]]
+    b = fasdfasd
     color = 0
     moves = gen_move(b,color)
     while True:
@@ -36,7 +36,6 @@ def main():
             print("Game over")
             break
         print(f"{PLAYERS[color]}'s turn:")
-        print(len(moves))
         m = get_move(b,moves,color)
         if m:
             b = m[0]
@@ -45,3 +44,16 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+D.update({0:[[4,8,6,2,0,6,8,4],[10]*8,[-1]*8,[-1]*8,[-1]*8,[-1]*8,[11]*8,[5,9,7,3,1,7,9,5]],1:0})or D.update({2,GM(D[0],D[1])})or any(
+        print(chr(27)+"c")or PB(D[0])or print()or not(input(),)
+        for _ in iter(int,1))
+
+MU,MC
+(lambda b,M:print(">> ", end="")or{NT(b,m[0],*m[1],*m[2]):m for m in M}.get(input().strip().lower(),0)),(lambda b,M:print("... Thinking ...")or M[NM(NM,b,M,2,-1e6,1e6,-1)[1]])
+
+
+NM
+lambda f,b,M,d,A,B,p,D={}:d==0 and(p*HF(b),0)or not M and(Fc(b,p<0)and (-1e6,0)or(0,0))or D.update({0:-1e7,1:-1,2:A,3:B})or[D.update({4:-f(f,m[0],GM(m[0],p>0),d-1,-D[3],-D[2],-p,{})[0]})or D[4]<=D[0]or D.update({0:D[4],1:i,2:max(D[2],D[4])})for i,m in enumerate(M)]and(D[0],D[1])
--- notation.py
+++ notation.py
@@ -12,7 +12,7 @@ B_KNIGHT = 9
 W_PAWN   = 10
 B_PAWN   = 11
 
-FILES = ["a","b","c","d","e","f","g","h"]
+FILES = ("a","b","c","d","e","f","g","h")
 PIECES = {
     W_QUEEN  //2: "q",
     W_ROOK   //2: "r",
@@ -25,3 +25,5 @@ def get_move_notation(b, b2, r, c, r2, c2):
     if b[r][c]//2==5 and r2%7==0: # pawn reaches 0 or 7 rank 
         s += f"={PIECES[b2[r2][c2]//2]}"
     return s
+
+NT=lambda b,B,r,c,R,C:f"{FILES[c]}{r+1}{FILES[c2]}{r2+1}"
